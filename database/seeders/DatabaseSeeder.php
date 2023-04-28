281603c68fbd433b58e8e5e18a402dced016e2a2
arari3s <arlanariandi@gmail.com>
2023-04-28 09:53:17 +0700

    UPDATE: Update Update factory employee, responsibility, role and team Add tabel bantu many to many user and company Add seeder UserCompany

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
+use App\Models\Role;
 use App\Models\Team;
 use Illuminate\Database\Seeder;
 
@@ -23,7 +26,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
         // Company::factory(10)->create();
-        Team::factory(30)->create();
+        // Team::factory(30)->create();
+        // Role::factory(50)->create();
+        // Responsibility::factory(150)->create();
+        // Employee::factory(1000)->create();
     }
 }
