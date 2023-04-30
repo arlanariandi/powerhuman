f3e23e64c072da2d5c6e9d92bd52a2c598cecfcc
arari3s <arlanariandi@gmail.com>
2023-04-30 13:28:15 +0700

    UPDATE: Update API register in usercontroller

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,6 +9,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
 {
@@ -33,16 +34,49 @@ class UserController extends Controller
             }
 
             // Generate token
-            $toketResult = $user->createToken('authToken')->plainTextToken;
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
 
             // Return response
             return ResponseFormatter::success([
-                'access_token' => $toketResult,
+                'access_token' => $tokenResult,
                 'token_type' => 'Bearer',
                 'user' => $user,
             ], 'Login success');
-        } catch (Exception $e) {
+        } catch (Exception $error) {
+            // Return error response
             return ResponseFormatter::error('Authentication Failed');
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            // Validate request
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', Password::min(8)],
+            ]);
+
+            // Create user
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            // Generate token
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            // Return response
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ], 'Login success');
+        } catch (Exception $error) {
+            // Return error response
+            return ResponseFormatter::error($error->getMessage());
         }
     }
 }
