3bf633a2eece0e231179dcb84f468f2b8f03d8b1
arari3s <arlanariandi@gmail.com>
2023-05-03 06:12:46 +0700

    UPDATE: Update attach company to user and load user at company

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -6,8 +6,10 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateCompanyRequest;
 use App\Models\Company;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CompanyController extends Controller
 {
@@ -43,10 +45,12 @@ class CompanyController extends Controller
     public function create(CreateCompanyRequest $request)
     {
         try {
+            // Upload logo
             if ($request->hasFile('logo')) {
                 $path = $request->file('logo')->store('public/logos');
             }
 
+            // Create company
             $company = Company::create([
                 'name' => $request->name,
                 'logo' => $path,
@@ -55,6 +59,13 @@ class CompanyController extends Controller
             if (!$company) {
                 throw new Exception('Company not created');
             }
+
+            // Attach company to user
+            $user = User::find(Auth::id());
+            $user->companies()->attach($company->id);
+
+            // Load user at company
+            $company->load('users');
 
             return ResponseFormatter::success($company, 'Company created');
         } catch (Exception $error) {
